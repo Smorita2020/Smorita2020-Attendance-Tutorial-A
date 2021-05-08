0939aa8acacf5a7be867fa86df87df60d2f3b553
Smorita2020 <inoshikacho385@gmail.com>
2021-05-08 01:20:48 +0000

    バリデーション（出社のみの更新ができない）を追加

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -8,6 +8,8 @@ class Attendance < ApplicationRecord
   validate :finished_at_is_invalid_without_a_started_at
   # 出勤・退勤時間どちらも存在する時、出勤時間より早い退勤時間は無効
   validate :started_at_than_finished_at_fast_if_invalid
+  # 退勤時間が存在しない場合、出勤時間は無効
+  validate :started_at_is_invalid_without_a_finished_at
   
   def finished_at_is_invalid_without_a_started_at
     errors.add(:started_at, "が必要です") if started_at.blank? && finished_at.present?
@@ -16,6 +18,14 @@ class Attendance < ApplicationRecord
   def started_at_than_finished_at_fast_if_invalid
     if started_at.present? && finished_at.present?
       errors.add(:started_at, "より早い退社時間は無効です") if started_at > finished_at
+    end
+  end
+  
+  def started_at_is_invalid_without_a_finished_at
+    unless Date.current == worked_on
+      if started_at.present? && finished_at.blank?
+        errors.add(:finished_at, "が必要です。") 
+      end
     end
   end
 end
