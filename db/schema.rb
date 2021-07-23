62d55f5404437308b82e179184e3a70da5a7c683
EC2 Default User <ec2-user@ip-172-31-28-90.ap-northeast-1.compute.internal>
2021-07-23 09:31:36 +0000

    上長カラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210722230451) do
+ActiveRecord::Schema.define(version: 20210723092653) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20210722230451) do
     t.string "affiliation"
     t.integer "employee_number"
     t.string "uid"
+    t.boolean "superior", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
